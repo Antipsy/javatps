12aad5ac3443da5efa92feeb2fb9186b5deb72e1
Antipsy <wassim.elbakbachi@emsi-edu.ma>
2023-01-02 20:46:54 +0100

    problem tp2 ex4 fixé

--- src/main/java/jsf/Bean2.java
+++ src/main/java/jsf/Bean2.java
@@ -26,7 +26,7 @@ private int nombre;
         this.nombre = nombre;
     }
     public String afficher() {
-    return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+    return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
     }
     public List<Integer> getNombresSuivants() {
         int nb = 5;
